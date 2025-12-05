645aca675dca18f759c17b2252fe71c446d59cee
mrickyrizaldi <mrickyrizaldi@gmail.com>
2025-12-05 17:16:23 +0800

    Enhance output_test.ino with relay and buzzer logic
    Updated output_test.ino to include relay control and improved LED and buzzer functionality.

--- Pengujian_Komponen/output_test.ino
+++ Pengujian_Komponen/output_test.ino
@@ -1,26 +1,73 @@
-// Pin output
+const int relay = 6;
+const int buzzer = 7;
 const int led_merah = 8;
-const int buzzer    = 9;
 
 void setup() {
   Serial.begin(9600);
-
-  pinMode(led_merah, OUTPUT);
+  pinMode(relay, OUTPUT);
   pinMode(buzzer, OUTPUT);
+  pinMode(led_merah, OUTPUT);
 
-  // Awalnya mati
-  digitalWrite(led_merah, LOW);
+  // kondisi awal
+  digitalWrite(relay, LOW);
   digitalWrite(buzzer, LOW);
+  digitalWrite(led_merah, LOW);
 }
 
 void loop() {
-  // Nyalakan LED dan buzzer blink
-  digitalWrite(led_merah, HIGH);
-  digitalWrite(buzzer, HIGH);
-  delay(500);
+  // test LED Heartbeat
+  Serial.println("Mode Standby");
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(buzzer, LOW);
+    digitalWrite(relay, LOW);
 
-  // Matikan LED dan buzzer
-  digitalWrite(led_merah, LOW);
+    digitalWrite(led_merah, HIGH);
+    delay(100);
+    digitalWrite(led_merah, LOW);
+    delay(1900);
+  }
+
+  // test LED ON, buzzer beep jarang, relay OFF
+  Serial.println("Mode Gas Terdeteksi");
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(led_merah, HIGH);
+    digitalWrite(relay, LOW);
+
+    digitalWrite(buzzer, HIGH);
+    delay(500);
+    digitalWrite(buzzer, LOW);
+    delay(2000);
+  }
+
+  // test LED dan buzzer blink cepat serta relay OFF
+  Serial.println("Mode Api Terdeteksi");
+  for (int i = 0; i < 10; i++) {
+    digitalWrite(relay, LOW);
+
+    digitalWrite(led_merah, HIGH);
+    digitalWrite(buzzer, HIGH);
+    delay(500);
+
+    digitalWrite(led_merah, LOW);
+    digitalWrite(buzzer, LOW);
+    delay(500);
+  }
+
+  // test buzzer terus, LED blink sedang, relay ON
+  Serial.println("Mode Gas + Api");
+  for (int i = 0; i < 10; i++) {
+    digitalWrite(relay, HIGH);
+    digitalWrite(buzzer, HIGH);
+
+    digitalWrite(led_merah, HIGH);
+    delay(300);
+    digitalWrite(led_merah, LOW);
+    delay(300);
+  }
+
+  // Matikan semua
+  digitalWrite(relay, LOW);
   digitalWrite(buzzer, LOW);
-  delay(500);
+  digitalWrite(led_merah, LOW);
+  delay(3000);
 }
