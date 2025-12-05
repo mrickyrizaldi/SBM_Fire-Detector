d97042c67dc4722de84d6f3237031b1a0405a3d8
mrickyrizaldi <mrickyrizaldi@gmail.com>
2025-12-05 17:24:29 +0800

    add relay logic

--- Integrasi/Fire_detector.ino
+++ Integrasi/Fire_detector.ino
@@ -3,8 +3,9 @@ const int sensor_gas = A0;
 const int sensor_api = 3;
 
 // Output
+const int relay     = 6;
+const int buzzer    = 7;
 const int led_merah = 8;
-const int buzzer    = 9;
 
 // Variabel
 float sensorGas_Value;
@@ -16,11 +17,14 @@ void setup() {
   Serial.begin(9600);
   pinMode(sensor_gas, INPUT);
   pinMode(sensor_api, INPUT);
-  pinMode(led_merah, OUTPUT);
+  pinMode(relay, OUTPUT);
   pinMode(buzzer, OUTPUT);
+  pinMode(led_merah, OUTPUT);
 
-  digitalWrite(led_merah, LOW);
+  // kondisi awal
+  digitalWrite(relay, LOW);
   digitalWrite(buzzer, LOW);
+  digitalWrite(led_merah, LOW);
 }
 
 void loop() {
@@ -32,45 +36,51 @@ void loop() {
   bool gasTerdeteksi = sensorGas_Value > ambangGas;
   bool apiTerdeteksi = (sensorApi_Value == 1);
 
+  // Debug ke Serial Monitor
   Serial.print("Gas: ");
   Serial.println(sensorGas_Value);
   Serial.print("Api: ");
   Serial.println(sensorApi_Value);
-  Serial.println(); 
+  Serial.println();
 
   if (!gasTerdeteksi && !apiTerdeteksi) {
-    // stand by
+    // standby
     digitalWrite(buzzer, LOW);
-    
+    digitalWrite(relay, LOW);
+
     digitalWrite(led_merah, HIGH);
     delay(100);
     digitalWrite(led_merah, LOW);
-    delay(2900);
+    delay(1900);
   }
 
   else if (gasTerdeteksi && !apiTerdeteksi) {
     // Gas terdeteksi
+    digitalWrite(relay, LOW);
     digitalWrite(led_merah, HIGH);
 
     digitalWrite(buzzer, HIGH);
-    delay(200);
+    delay(500);
     digitalWrite(buzzer, LOW);
-    delay(1300);
+    delay(2000);
   }
 
   else if (!gasTerdeteksi && apiTerdeteksi) {
     // Api terdeteksi
+    digitalWrite(relay, LOW);
+
     digitalWrite(led_merah, HIGH);
     digitalWrite(buzzer, HIGH);
-    delay(200);
+    delay(500);
 
     digitalWrite(led_merah, LOW);
     digitalWrite(buzzer, LOW);
-    delay(200);
+    delay(500);
   }
 
   else {
     // Gas dan api terdeteksi secara bersamaan
+    digitalWrite(relay, HIGH);
     digitalWrite(buzzer, HIGH);
 
     digitalWrite(led_merah, HIGH);
