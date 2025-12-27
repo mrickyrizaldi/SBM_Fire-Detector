a4b98cf06993062ca025420c139ab6ca2fb9607d
mrickyrizaldi <mrickyrizaldi@gmail.com>
2025-12-28 07:42:24 +0800

    Refactor output_test.ino for pin assignments and logic
    Updated pin assignments and modified serial communication speed. Simplified loop logic for LED, buzzer, and relay control.

--- Pengujian_Komponen/output_test.ino
+++ Pengujian_Komponen/output_test.ino
@@ -1,73 +1,28 @@
-const int relay = 6;
-const int buzzer = 7;
-const int led_merah = 8;
+const int led_merah = 33;
+const int buzzer    = 25;
+const int relay     = 26;
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(relay, OUTPUT);
-  pinMode(buzzer, OUTPUT);
+  Serial.begin(115200);
   pinMode(led_merah, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+  pinMode(relay, OUTPUT);
 
-  // kondisi awal
-  digitalWrite(relay, LOW);
-  digitalWrite(buzzer, LOW);
   digitalWrite(led_merah, LOW);
+  digitalWrite(buzzer, LOW);
+  digitalWrite(relay, LOW);
 }
 
 void loop() {
-  // test LED Heartbeat
-  Serial.println("Mode Standby");
-  for (int i = 0; i < 3; i++) {
-    digitalWrite(buzzer, LOW);
-    digitalWrite(relay, LOW);
+  digitalWrite(led_merah, HIGH);
+  digitalWrite(buzzer, HIGH);
+  digitalWrite(relay, HIGH);
+  Serial.println("LED, Buzzer, Relay: ON");
+  delay(500);
 
-    digitalWrite(led_merah, HIGH);
-    delay(100);
-    digitalWrite(led_merah, LOW);
-    delay(1900);
-  }
-
-  // test LED ON, buzzer beep jarang, relay OFF
-  Serial.println("Mode Gas Terdeteksi");
-  for (int i = 0; i < 5; i++) {
-    digitalWrite(led_merah, HIGH);
-    digitalWrite(relay, LOW);
-
-    digitalWrite(buzzer, HIGH);
-    delay(500);
-    digitalWrite(buzzer, LOW);
-    delay(2000);
-  }
-
-  // test LED dan buzzer blink cepat serta relay OFF
-  Serial.println("Mode Api Terdeteksi");
-  for (int i = 0; i < 10; i++) {
-    digitalWrite(relay, LOW);
-
-    digitalWrite(led_merah, HIGH);
-    digitalWrite(buzzer, HIGH);
-    delay(500);
-
-    digitalWrite(led_merah, LOW);
-    digitalWrite(buzzer, LOW);
-    delay(500);
-  }
-
-  // test buzzer terus, LED blink sedang, relay ON
-  Serial.println("Mode Gas + Api");
-  for (int i = 0; i < 10; i++) {
-    digitalWrite(relay, HIGH);
-    digitalWrite(buzzer, HIGH);
-
-    digitalWrite(led_merah, HIGH);
-    delay(300);
-    digitalWrite(led_merah, LOW);
-    delay(300);
-  }
-
-  // Matikan semua
-  digitalWrite(relay, LOW);
-  digitalWrite(buzzer, LOW);
   digitalWrite(led_merah, LOW);
-  delay(3000);
+  digitalWrite(buzzer, LOW);
+  digitalWrite(relay, LOW);
+  Serial.println("LED, Buzzer, Relay: OFF");
+  delay(500);
 }
